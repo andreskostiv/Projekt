df30fd2ec7843e4ea7a55032d3a045d9a1da26a0
ANDRES KOSTIV <andres.kostiv@gmail.com>
2015-11-17 22:06:22 +0200

    kolm
    kolm

---
+++ Kaivitaja.java
@@ -0,0 +1,18 @@
+package Projekt;
+
+// See käivitab programmi
+
+public class Kaivitaja {
+
+    public static void main(String[] args) {
+
+
+       Generaator a = new Generaator(); // lõin uue "a" objekti
+
+        a.Algus();
+
+
+
+
+    }
+}
